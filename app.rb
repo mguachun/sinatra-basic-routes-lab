ccb24afe47c5e6b30ca018fdce459e48317473f9
Melissa Guachun <1212malka@gmail.com>
2021-04-01 18:11:21 -0400

    commit tree abfbdc8cab83bc1abc1a67fbd4b88c3170ee6f39

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Melissa"
+      end 
+
+      get '/hometown' do
+        "My hometown is Forest Hills, Queens"
+      end
+
+      get '/favorite-song' do
+        "My favorite song is Resilient Bastard by Shellshag"
+      end
 end
